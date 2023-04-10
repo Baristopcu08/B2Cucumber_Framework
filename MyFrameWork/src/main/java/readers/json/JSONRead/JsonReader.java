18c62c756eb6a2a1e051688e03e829991851db55
Baristopcu08 <112826011+Baristopcu08@users.noreply.github.com>
2023-04-10 21:35:19 +0300

    10.04.2023

--- MyFrameWork/src/main/java/readers/json/JSONRead/JsonReader.java
+++ MyFrameWork/src/main/java/readers/json/JSONRead/JsonReader.java
@@ -1,4 +1,16 @@
 package readers.json.JSONRead;
 
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class JsonReader {
+
+    public static void main(String[] args) throws FileNotFoundException {
+        String file ="src/test/resources/datafiles/configME.json";
+        JSONObject parse = (JSONObject) JSONValue.parse(new FileReader(file));
+        System.out.println(parse.toJSONString());
+    }
 }
